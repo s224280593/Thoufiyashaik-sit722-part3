f5c196e60290303b324ee8cf36ed7ff427ced3cc
s224280593 <162409765+s224280593@users.noreply.github.com>
2024-09-23 23:44:15 +1000

    initial commit

--- project_part3/book_catalog/app/database.py
+++ project_part3/book_catalog/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://thoufiya_sit722_part3_fgz4_user:gO06rHRjQ3R7TTT1iNrIp0txA6OHUXsq@dpg-crom97q3esus73c3g1h0-a.oregon-postgres.render.com/thoufiya_sit722_part3_fgz4" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- project_part3/inventory_management/app/config.py
+++ project_part3/inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://thoufiya_sit722_part3_fgz4_user:gO06rHRjQ3R7TTT1iNrIp0txA6OHUXsq@dpg-crom97q3esus73c3g1h0-a.oregon-postgres.render.com/thoufiya_sit722_part3_fgz4')
